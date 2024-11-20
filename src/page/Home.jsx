21bfca4ce97f7d4c2b9c6cbb4c0e7fe80178af2d
Nath2006 <ananthapindika739@gmail.com>
2024-11-20 21:01:09 +0700

    Feat : Add new layout and style in Homepage

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,11 +2,27 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <a href="BMI">BMI</a>
-      <a href="Kecepatan">Kecepatan</a>
-      <a href="Suhu">Suhu</a>
-      <a href="Diskon">Diskon</a>
+    <div className="flex justify-center items-center bg-black min-h-screen text-white">
+      <div className="grid grid-cols-2 gap-4">
+
+        <div className="flex justify-center items-center bg-gray-800 rounded-lg w-40 h-40 text-center">
+          <a href="BMI" className="text-xl font-bold">BMI</a>
+        </div>
+
+
+        <div className="flex justify-center items-center bg-gray-800 rounded-lg w-40 h-40 text-center">
+          <a href="Kecepatan" className="text-xl font-bold">Kecepatan</a>
+        </div>
+
+
+        <div className="flex justify-center items-center bg-gray-800 rounded-lg w-40 h-40 text-center">
+          <a href="Suhu" className="text-xl font-bold">Suhu</a>
+        </div>
+
+        <div className="flex justify-center items-center bg-gray-800 rounded-lg w-40 h-40 text-center">
+          <a href="Diskon" className="text-xl font-bold">Diskon</a>
+        </div>
+      </div>
     </div>
   );
 }
